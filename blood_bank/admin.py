29dccf2d4d9a6069ac51eee15affa35ff0246cb7
Ibtehaz <ibtehaz.shawon@gmail.com>
2018-05-09 03:02:04 +0600

    adding flow controller in admin table

--- blood_bank/admin.py
+++ blood_bank/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import UserInformation,UserStatus,ErrorLogger,DonationHistory
-
+from .models import UserInformation, UserStatus, ErrorLogger, DonationHistory, FlowController
 
 # Register your models here.
 
@@ -8,3 +7,4 @@ admin.site.register(UserInformation)
 admin.site.register(UserStatus)
 admin.site.register(DonationHistory)
 admin.site.register(ErrorLogger)
+admin.site.register(FlowController)
